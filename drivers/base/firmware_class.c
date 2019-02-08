edaa75c4ac19650e46a913026df17d7fd7ecce9e
Alireza Barani <46503882+alireza1219@users.noreply.github.com>
2019-02-08 06:18:16 -0500

    Update firmware_class.c
    Maybe it would be helpful to fix this error:
    " error: too few arguments to function 'fw_load_from_user_helper'
       ret = fw_load_from_user_helper(fw, desc, timeout); "

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -1251,12 +1251,22 @@ static int _request_firmware(struct fw_desc *desc)
 		}
 	}
 
-	if (!fw_get_filesystem_firmware(desc->device, fw->priv,
-					desc->dest_addr, desc->dest_size))
-		ret = fw_load_from_user_helper(fw, desc, timeout);
-	if (!ret)
-		ret = assign_firmware_buf(fw, desc->device, desc->nocache);
+        ret = fw_get_filesystem_firmware(desc->device, fw->priv,
+					 desc->dest_addr, desc->dest_size);
+	if (ret) {
+		if (!(desc->opt_flags & FW_OPT_NO_WARN))
+			dev_dbg(desc->device,
+				 "Direct firmware load for %s failed with error %d\n",
+				 desc->name, ret);
+		if (desc->opt_flags & FW_OPT_USERHELPER) {
+			dev_dbg(desc->device, "Falling back to user helper\n");
+			ret = fw_load_from_user_helper(fw, desc, timeout);
+		}
+	}
 
+	if (!ret)
+                ret = assign_firmware_buf(fw, desc->device, desc->opt_flags);
+	
 	usermodehelper_read_unlock();
 
  out:
